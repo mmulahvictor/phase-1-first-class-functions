f0005418389b1d111a6f38afee5ce74b258b287e
mmulahvictor <mmulahvictor@gmail.com>
2022-06-08 08:26:20 +0300

    commit tree f134db415a4b549c3432390794f3e5ce243c9ec9

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction(callback){
+    return callback();
+}
+function returnsANamedFunction(){
+    return function namedFn(){
+        console.log('This is a named function');
+    }
+}
+function returnsAnAnonymousFunction(){
+    return () => console.log('This is an anonymous function');
+}
